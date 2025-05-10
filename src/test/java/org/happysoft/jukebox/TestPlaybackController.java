38d242fe806198198ef0fbd1b6574bfc74924012
ThunderSOS <c_francis1@yahoo.com>
2025-05-10 17:05:44 +0100

    Removed some auto generated comments

--- src/test/java/org/happysoft/jukebox/TestPlaybackController.java
+++ src/test/java/org/happysoft/jukebox/TestPlaybackController.java
@@ -1,17 +1,10 @@
 package org.happysoft.jukebox;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
 import java.io.File;
 import junit.framework.TestCase;
 import org.happysoft.jbplayer.*;
 
 /**
- *
  * @author chrisf
  */
 public class TestPlaybackController extends TestCase implements AudioPlaybackListener {
